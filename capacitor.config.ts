1cdff8dead7afc1f2a42eed208274a9a4c09147e
Roger Bernar <roger.bernar9@gmail.com>
2023-05-08 15:28:59 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'swiggyapp',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
